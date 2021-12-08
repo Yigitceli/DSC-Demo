85db9290f35fcb09a36e38a4421ef5ab7e0ff15c
Yigitceli <yigit.celi@outlook.com>
2021-12-08 16:04:09 +0300

    finalized

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { auth, db } from "./config/firebase-config";
 import { GoogleAuthProvider, signInWithRedirect } from "@firebase/auth";
 import Navbar from "./components/Navbar";
 import Authenticate from "./pages/Authenticate";
+import ReactLoading from "react-loading";
 import { doc, setDoc, getDoc, collection } from "firebase/firestore";
 
 import { useEffect, useState } from "react";
@@ -12,13 +13,16 @@ const provider = new GoogleAuthProvider();
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
   const loginWithGoogle = () => {
     signInWithRedirect(auth, provider);
   };
   useEffect(() => {
+    setIsLoading(true);
     auth.onAuthStateChanged(async (user) => {
       setUser(user);
+
       if (user) {
         await setDoc(doc(db, "Users", user?.uid), {
           firstName: user.displayName.split(" ")[0],
@@ -29,19 +33,29 @@ function App() {
         const todosRef = doc(db, "Todos", user?.uid);
         const todosSnap = await getDoc(todosRef);
 
-        if (!todosSnap.exists()) {          
+        if (!todosSnap.exists()) {
           await setDoc(doc(db, "Todos", user?.uid), { todos: [] });
         }
 
         setIsLoggedIn(true);
       }
+      setIsLoading(false);
     });
   }, [user]);
 
   return (
     <div className="App">
-      <Navbar pp={user?.photoURL} setIsLoggedIn={setIsLoggedIn} />
-      {!isLoggedIn ? (
+      <Navbar
+        pp={user?.photoURL}
+        user={user}
+        setIsLoggedIn={setIsLoggedIn}
+        setIsLoading={setIsLoading}
+      />
+      {isLoading ? (
+        <div className='d-flex justify-content-center align-items-center w-100 h-100'>
+          <ReactLoading type={"spin"} color={"red"} height={200} width={150} />
+        </div>
+      ) : !user ? (
         <Authenticate loginWithGoogle={loginWithGoogle} />
       ) : (
         <Todos pp={user?.photoURL} user={user} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,15 +10,20 @@ import {
   signOut,
 } from "@firebase/auth";
 
-export default function Navbar({ pp, setIsLoggedIn }) {
+export default function Navbar({ pp, setIsLoggedIn, user, setIsLoading }) {
   const logOut = () => {
     setIsLoggedIn(false);
-    signOut(auth).then(() => {});
+    signOut(auth).then(() => {
+      setIsLoading(false);
+    });
   };
   return (
-    <nav className="navbar navbar-light bg-dark p-3" style={{ height: "15vh" }}>
-      <div className=" d-flex align-items-center w-100">
-        <div>
+    <nav className="navbar navbar-light bg-dark p-3">
+      <div
+        className=" d-flex align-items-center w-100"
+        style={{ height: "15vh" }}
+      >
+        <div style={{ flex: 1 }}>
           {pp && (
             <img
               src={pp}
@@ -29,14 +34,16 @@ export default function Navbar({ pp, setIsLoggedIn }) {
         </div>
         <span
           style={{ flex: 1 }}
-          className="navbar-brand mb-0 m-0 h1 text-danger fs-1 text-end"
+          className="navbar-brand mb-0 m-0 h1 text-danger text-center fs-1 text-end"
         >
           Todos
         </span>
         <div style={{ flex: 1 }} className="text-end">
-          <button type="button" className="btn btn-light" onClick={logOut}>
-            Logout
-          </button>
+          {user && (
+            <button type="button" className="btn btn-light" onClick={logOut}>
+              Logout
+            </button>
+          )}
         </div>
       </div>
     </nav>
